b64fe95fb0931d6ae40a13ca3227848b21ceff92
Boris <borisonekenobi@gmail.com>
2023-02-21 15:41:02 -0500

    Feb 21, 2023

--- scripts/import-models.js
+++ scripts/import-models.js
@@ -1,4 +1,4 @@
-import objects from "./objects.json" assert { type: "json" };
+import objects from "../JSONs/models.json" assert { type: "json" };
 
 let mainDiv = document.getElementById("designs");
 objects.forEach(object => {
@@ -6,7 +6,7 @@ objects.forEach(object => {
 
     let imgDiv = document.createElement('div');
     let img = document.createElement('img');
-    img.src = object.img != undefined ? object.img : "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftacm.com%2Fwp-content%2Fuploads%2F2018%2F01%2Fno-image-available.jpeg&f=1&nofb=1&ipt=71bb003925e70b7ee385616f7700f20ef9c60b95a59cd36780da6a5d624e862d&ipo=images";
+    img.src = object.img != undefined ? object.img : "../resources/no-image-available.png";
     img.alt = object.title;
     imgDiv.appendChild(img);
 
---
+++ scripts/import-partners.js
@@ -0,0 +1,62 @@
+import partners from "../JSONs/partners.json" assert { type: "json" };
+
+let mainDiv = document.getElementById("partners");
+partners.forEach(partner => {
+    let elemDiv = document.createElement('div');
+
+    let imgDiv = document.createElement('div');
+    let img = document.createElement('img');
+    imgDiv.id = 'img';
+    img.src = partner.img != undefined ? partner.img : "../resources/default-user.png";
+    img.alt = partner.name;
+    imgDiv.appendChild(img);
+
+    let name = document.createElement('p');
+    name.id = "name";
+    name.append(partner.name);
+
+    let socials = document.createElement('div');
+    socials.id = "socials";
+    if (partner.website != undefined) {
+        let website = document.createElement('a');
+        website.id = 'website';
+        website.href = partner.website;
+        socials.appendChild(website);
+    }
+    if (partner.youtube != undefined) {
+        let youtube = document.createElement('a');
+        youtube.id = 'youtube';
+        youtube.href = partner.youtube;
+        socials.appendChild(youtube);
+    }
+    if (partner.instagram != undefined) {
+        let instagram = document.createElement('a');
+        instagram.id = 'instagram';
+        instagram.href = partner.instagram;
+        socials.appendChild(instagram);
+    }
+    if (partner.discord != undefined) {
+        let discord = document.createElement('a');
+        discord.id = 'discord';
+        discord.href = partner.discord;
+        socials.appendChild(discord);
+    }
+    if (partner.twitter != undefined) {
+        let twitter = document.createElement('a');
+        twitter.id = 'twitter';
+        twitter.href = partner.twitter;
+        socials.appendChild(twitter);
+    }
+    if (partner.facebook != undefined) {
+        let facebook = document.createElement('a');
+        facebook.id = 'facebook';
+        facebook.href = partner.facebook;
+        socials.appendChild(facebook);
+    }
+
+    elemDiv.appendChild(imgDiv);
+    elemDiv.appendChild(name);
+    elemDiv.appendChild(socials);
+
+    mainDiv.appendChild(elemDiv);
+});
