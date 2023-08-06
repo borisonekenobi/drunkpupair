90577a8872d4d309b456d4b09e253dae2217680d
drunkpupper25 <drunk.pupper25@gmail.com>
2023-08-06 10:20:51 +0200

    imgs and model.js

--- JSONs/models.js
+++ JSONs/models.js
@@ -1,115 +1,136 @@
 export default
   [
     {
-      "title": "[4k] MALEV Hungarian Airlines HA-LAX Airbus A310-300",
-      "link": "../downloads/TP a310-livery-MALEV_EpKcZ.zip",
-      "img": "../resources/models/310-malev.png",
-      "version": "1.0",
-      "lastUpdated": "02/04/23"
-    },
-    {
-      "title": "[Ultra] MALEV Hungarian Airlines HA-LHX Boeing 787-10",
-      "link": "../downloads/TP-livery-b787-MALEV HA-LHX_RSo8b.zip",
-      "img": "../resources/models/787-malev.png",
-      "version": "2.0",
-      "lastUpdated": "02/04/23"
-    },
-    {
-      "title": "[Ultra] MALEV Hungarian Airlines HA-LAD Asobo Airbus A320neo",
-      "img": "../resources/models/malev-320.PNG",
+      "title": "[Ultra] MALEV HA-LAD Asobo A320neo",
+      "img": "../resources/models/A320 MALEV.png",
       "link": "../downloads/TP-a320-livery-TPMALEV-V3_lKUvQ.zip",
       "version": "3.0",
       "lastUpdated": "02/04/23"
     },
     {
-      "title": "[Ultra] MALEV Hungarian Airlines VA HA-LAC & -LAD FBW A32NX",
-      "img": "../resources/models/malev-320.PNG",
+      "title": "[Ultra] MALEV VA HA-LAC & -LAD FBW A32NX",
+      "img": "../resources/models/A320 MALEV.png",
       "link": "../downloads/fbw A32NX-livery-TPMALEV-V3 HA-LAD_iqQC0.zip",
       "version": "3.5",
-      //"lastUpdated": "tbd"
-    },
+      "lastUpdated": "06/08/23"
+    },    
     {
       "title": "[Ultra] MALEV Retro il-14 style VA HA-LAE FBW A32NX",
-      //"img": "../resources/models/tbd",
+      "img": "../resources/models/A320 HA-LAE.png",
       //"link": "../downloads/tbd",
       "version": "1.3",
-      //"lastUpdated": "tbd"
+      "lastUpdated": "06/08/23"
     },
     {
-      "title": "[8k] Hungarian Air Force 604 & 605 FBW A32NX",
-      "link": "../downloads/FBW HUNAF 604 n 605_gdQJ5.zip",
-      "img": "../resources/models/hunaf-a320.JPG",
-      "version": "1.1",
-      "lastUpdated": "02/04/23"
-    },
-    {
-      "title": "[Ultra] MALEV Hungarian Airlines VA HA-LHG Boeing 747-8i",
+      "title": "[Ultra] MALEV VA HA-LHG Asobo 747-8i",
       "link": "../downloads/Asobo_B747_8i_MALEV v2.0_0iZCW.zip",
-      "img": "../resources/models/747-malev.png",
+      "img": "../resources/models/747 MALEV.png",
       "version": "2.2",
       "lastUpdated": "02/04/23"
     },
     {
-      "title": "[8k] Airbus ACJ320neo SCM P4-RLA FBW A32NX",
-      "link": "../downloads/FBW A32NX SCM liveries_euwbQ.zip",
-      "img": "../resources/models/SCM-a320.JPG",
-      "version": "1.1",
+      "title": "[Ultra] MALEV VA HA-LHG Salty 747-8",
+      //"link": "../downloads/",
+      "img": "../resources/models/747S MALEV2.png",
+      "version": "1.0",
       "lastUpdated": "02/04/23"
     },
     {
-      "title": "[8k] Airbus ACJ320neo Global Jet Luxembourg M-RBUS FBW A32NX",
-      "link": "../downloads/FBW A32NX liveries Global Jet Luxembourg_34CIe.zip",
-      "img": "../resources/models/Global-jets-Lux-a320.JPG",
-      "version": "1.2",
+      "title": "[8k] MALEV VA HA-LCS & -LCP & -LCT Cessna C208B EX",
+      //"link": "../downloads/TPcessna208b-MALEV_I6aio.zip",
+      "img": "../resources/models/C208 MALEV.png",
+      "version": "2.0",
       "lastUpdated": "02/04/23"
     },
     {
-      "title": "[8k] Airbus ACJ320neo Sophar M-KATE FBW A32NX",
-      "link": "../downloads/FBW A32NX liveries Sophar_iSNct.zip",
-      "img": "../resources/models/M-kate-a320.JPG",
-      "version": "1.2",
-      "lastUpdated": "02/04/23"
+      "title": "[8k] MALEV VA HA-LAT ATR 72-600",
+      "img": "../resources/models/ATR MALEV.png",
+      //"link": "../downloads/tbd",
+      "version": "1.0",
+      "lastUpdated": "06/08/23"
     },
     {
-      "title": "[8k] Airbus ACJ320neo Comlux Aviation 9H-NEW FBW A32NX",
-      "link": "../downloads/FBW A32NX-livery-Comlux Aviation_AGCzM.zip",
-      "img": "../resources/models/comlux-a320.JPG",
-      "version": "1.2",
-      "lastUpdated": "02/04/23"
+      "title": "[4k] MALEV VA HA-FRD Ford Trimotor 4-AT",
+      "img": "../resources/models/HA-FRD.png",
+      //"link": "../downloads/tbd",
+      "version": "1.0",
+      "lastUpdated": "06/08/23"
     },
     {
-      "title": "[8k] MALEV VA HA-LCS & -LCP & -LCT Cessna C208",
-      "link": "../downloads/TPcessna208b-MALEV_I6aio.zip",
-      //"img": "../resources/models/tbd",
+      "title": "[Ultra] MALEV HA-LHX Asobo 787-10",
+      "link": "../downloads/TP-livery-b787-MALEV HA-LHX_RSo8b.zip",
+      //"img": "../resources/models/787-malev.png",
       "version": "2.0",
       "lastUpdated": "02/04/23"
     },
     {
       "title": "[8k] MALEV Cessna Citation CJ4",
       "link": "../downloads/TPcj4-MALEV_ce2so.zip",
-      "img": "../resources/models/malev-cj4.JPG",
+      //"img": "../resources/models/malev-cj4.JPG",
       "version": "1.0",
       "lastUpdated": "02/04/23"
     },
     {
       "title": "[8k] MALEV HA-TBM Daher TBM930",
       "link": "./downloads/TPtbm-livery-MALEV_cqvLW.zip",
-      "img": "../resources/models/malev-TBM.png",
+      //"img": "../resources/models/malev TBM.png",
       "version": "2.0",
       "lastUpdated": "02/04/23"
     },
     {
       "title": "[8k] MALEV HA-SJX Cessna C172(G1000)",
       "link": "../downloads/TP172-MALEV PS_P4exw.zip",
-      "img": "../resources/models/HA-SJX.JPG",
+      //"img": "../resources/models/HA-SJX.JPG",
       "version": "1.0",
       "lastUpdated": "02/04/23"
     },
     {
       "title": "[8k] MALEV VA HA-LKA King Air 350",
       "link": "../downloads/TPKing Air 350 MALEV_Fy09H.zip",
-      "img": "../resources/models/KA-350i-malev.png",
+      "img": "../resources/models/HA-LAK.png",
       "version": "2.1",
+      "lastUpdated": "06/08/23"
+    },
+    {
+      "title": "[4k] MALEV  HA-LAX iniBuilds A310-300",
+      "link": "../downloads/TP a310-livery-MALEV_EpKcZ.zip",
+      //"img": "../resources/models/310-malev.png",
+      "version": "1.0",
+      "lastUpdated": "02/04/23"
+    },
+    {
+      "title": "[8k] Hungarian Air Force 604 & 605 FBW A32NX",
+      "link": "../downloads/FBW HUNAF 604 n 605_gdQJ5.zip",
+      //"img": "../resources/models/hunaf-a320.JPG",
+      "version": "1.1",
+      "lastUpdated": "02/04/23"
+    },
+    {
+      "title": "[8k] Airbus ACJ320neo SCM P4-RLA FBW A32NX",
+      "link": "../downloads/FBW A32NX SCM liveries_euwbQ.zip",
+      //"img": "../resources/models/SCM-a320.JPG",
+      "version": "1.1",
+      "lastUpdated": "02/04/23"
+    },
+    {
+      "title": "[8k] Airbus ACJ320neo Global Jet Luxembourg M-RBUS FBW A32NX",
+      "link": "../downloads/FBW A32NX liveries Global Jet Luxembourg_34CIe.zip",
+      //"img": "../resources/models/Global-jets-Lux-a320.JPG",
+      "version": "1.2",
+      "lastUpdated": "02/04/23"
+    },
+    {
+      "title": "[8k] Airbus ACJ320neo Sophar M-KATE FBW A32NX",
+      "link": "../downloads/FBW A32NX liveries Sophar_iSNct.zip",
+      //"img": "../resources/models/M-kate-a320.JPG",
+      "version": "1.2",
+      "lastUpdated": "02/04/23"
+    },
+    {
+      "title": "[8k] Airbus ACJ320neo Comlux Aviation 9H-NEW FBW A32NX",
+      "link": "../downloads/FBW A32NX-livery-Comlux Aviation_AGCzM.zip",
+      //"img": "../resources/models/comlux-a320.JPG",
+      "version": "1.2",
       "lastUpdated": "02/04/23"
     }
   ]
